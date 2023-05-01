bbbdcada886a887118af5f7a043960461ac3a372
mjambresic <matija.jambresic@yahoo.com>
2023-05-02 00:37:46 +0200

    just array things

--- Array2DPath/Program.cs
+++ Array2DPath/Program.cs
@@ -7,15 +7,7 @@ namespace Array2DPath
         public static void Main(string[] args)
         {
             char[,] pathArray = new char[8, 160];
-            
-            // populate
-            for (int x = 0; x < pathArray.GetLength(0); x++)
-            {
-                for (int y = 0; y < pathArray.GetLength(1); y++)
-                {
-                    pathArray[x, y] = '-';
-                }
-            }
+            PopulateArray2D(pathArray);
 
             Random random = new Random();
             int newIndex = random.Next(2, 8);
@@ -31,15 +23,50 @@ namespace Array2DPath
                     newIndex = random.Next(Math.Max(newIndex - 1, 2), Math.Min(newIndex + 2, 8));
             }
             
-            // write line
             for (int x = 0; x < pathArray.GetLength(0); x++)
-            {
                 for (int y = 0; y < pathArray.GetLength(1); y++)
+                {
+                    if (pathArray[x, y] != '-')
+                        continue;
+                    
+                    if (x + 1 == pathArray.GetLength(0 ) || pathArray[x + 1, y] != '-')
+                        continue;
+                    
+                    if (y + 1 == pathArray.GetLength(1 ) || pathArray[x, y + 1] != '-')
+                        continue;
+                    
+                    if (x + 1 == pathArray.GetLength(0 ) || 
+                        y + 1 == pathArray.GetLength(1 ) || 
+                        pathArray[x + 1, y + 1] != '-')
+                        continue;
+
+                    pathArray[x, y] = '|';
+                    pathArray[x + 1, y] = '|';
+                    pathArray[x, y + 1] = '|';
+                    pathArray[x + 1, y + 1] = '|';
+                }
+
+            WriteArray2DInConsole(pathArray);
+        }
+
+        public static void PopulateArray2D(char[,] array2D)
+        {
+            for (int x = 0; x < array2D.GetLength(0); x++)
+                for (int y = 0; y < array2D.GetLength(1); y++)
+                    array2D[x, y] = '-';
+        }
+
+        public static void WriteArray2DInConsole(char[,] array2D)
+        {
+            // write line
+            for (int x = 0; x < array2D.GetLength(0); x++)
+            {
+                for (int y = 0; y < array2D.GetLength(1); y++)
                 {
                     if (y == 0)
                         Console.WriteLine();
                     
-                    Console.Write(pathArray[x, y]);
+                    Console.Write(array2D[x, y]);
                 }
             }
         }
