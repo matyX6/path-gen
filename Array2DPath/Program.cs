61f71ff902b70e231cf672233bddfd991583ea92
mjambresic <matija.jambresic@yahoo.com>
2023-05-02 13:01:01 +0200

    shape updates

--- Array2DPath/Program.cs
+++ Array2DPath/Program.cs
@@ -15,38 +15,95 @@ namespace Array2DPath
             // assign path
             for (int y = 0; y < pathArray.GetLength(1); y++)
             {
-                pathArray[newIndex, y] = 'X';
-                pathArray[newIndex - 1, y] = 'X';
-                pathArray[newIndex - 2, y] = 'X';
+                pathArray[newIndex, y] = '°';
+                pathArray[newIndex - 1, y] = '°';
+                pathArray[newIndex - 2, y] = '°';
                 
                 if (y % 2 == 1)
                     newIndex = random.Next(Math.Max(newIndex - 1, 2), Math.Min(newIndex + 2, 8));
             }
             
+            // big block
             for (int x = 0; x < pathArray.GetLength(0); x++)
                 for (int y = 0; y < pathArray.GetLength(1); y++)
                 {
-                    if (pathArray[x, y] != '-')
+                    int rand = random.Next(0, 3);
+                    
+                    if (rand == 0 && FitBigBlock(x, y, pathArray))
                         continue;
                     
-                    if (x + 1 == pathArray.GetLength(0 ) || pathArray[x + 1, y] != '-')
+                    if (rand == 1 && FitHorizontalBlock(x, y, pathArray))
                         continue;
                     
-                    if (y + 1 == pathArray.GetLength(1 ) || pathArray[x, y + 1] != '-')
+                    if (rand == 2 && FitVerticalBlock(x, y, pathArray))
                         continue;
                     
-                    if (x + 1 == pathArray.GetLength(0 ) || 
-                        y + 1 == pathArray.GetLength(1 ) || 
-                        pathArray[x + 1, y + 1] != '-')
-                        continue;
-
-                    pathArray[x, y] = '|';
-                    pathArray[x + 1, y] = '|';
-                    pathArray[x, y + 1] = '|';
-                    pathArray[x + 1, y + 1] = '|';
+                    FitBlock(x, y, pathArray);
                 }
 
             WriteArray2DInConsole(pathArray);
+        }
+
+        public static bool FitBigBlock(int x, int y, char[,] array2D)
+        {
+            if (array2D[x, y] != '-')
+                return false;
+                    
+            if (x + 1 == array2D.GetLength(0 ) || array2D[x + 1, y] != '-')
+                return false;
+                    
+            if (y + 1 == array2D.GetLength(1 ) || array2D[x, y + 1] != '-')
+                return false;
+                    
+            if (x + 1 == array2D.GetLength(0 ) || 
+                y + 1 == array2D.GetLength(1 ) || 
+                array2D[x + 1, y + 1] != '-')
+                return false;
+
+            array2D[x, y] = '■';
+            array2D[x + 1, y] = '■';
+            array2D[x, y + 1] = '■';
+            array2D[x + 1, y + 1] = '■';
+
+            return true;
+        }
+        
+        public static void FitBlock(int x, int y, char[,] array2D)
+        {
+            if (array2D[x, y] != '-')
+                return;
+
+            array2D[x, y] = '▣';
+        }
+
+        public static bool FitHorizontalBlock(int x, int y, char[,] array2D)
+        {
+            if (array2D[x, y] != '-')
+                return false;
+            
+            if (y + 1 == array2D.GetLength(1 ) || 
+                array2D[x, y + 1] != '-')
+                return false;
+            
+            array2D[x, y] = '□';
+            array2D[x, y + 1] = '□';
+            
+            return true;
+        }
+        
+        public static bool FitVerticalBlock(int x, int y, char[,] array2D)
+        {
+            if (array2D[x, y] != '-')
+                return false;
+            
+            if (x + 1 == array2D.GetLength(0 ) || 
+                array2D[x + 1, y] != '-')
+                return false;
+            
+            array2D[x, y] = '▢';
+            array2D[x, y + 1] = '▢';
+            
+            return true;
         }
 
         public static void PopulateArray2D(char[,] array2D)
